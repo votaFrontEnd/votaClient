f0c5de317a25e9a476727333131ccf436aa4fafb
votaFrontEnd <38767672+votaFrontEnd@users.noreply.github.com>
2018-09-29 10:05:40 -0400

    add new applicant

--- src/reducers/applicantsReducer.js
+++ src/reducers/applicantsReducer.js
@@ -7,18 +7,7 @@ export default function applicantsReducer(state = [], action) {
       return action.applicants;
     case types.CREATE_APPLICANTS_SUCCESS:
       const newState = Object.assign([], state);
-      console.log(newState.length);
-      action.applicants.forEach(function(applicant) {
-        if (
-          newState.filter(existingApplicant => {
-            return existingApplicant.ID == applicant.ID;
-          }).length == 0
-        ) {
-          newState.push(applicant);
-        }
-      });
-      console.log(newState.length);
-      console.log("---");
+      newState.push(action.applicants);
       return newState;
   }
   return state;
